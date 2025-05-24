13f05214d831e5af4197320e91f9eba93fed5af9
miriimagallanes <miranda271127@gmail.com>
2025-05-24 12:55:27 -0300

    excepciones

--- errores.py
+++ errores.py
@@ -1,38 +1,22 @@
-# --- DONANTE --- #
+class Paciente_No_Encontrado:
+    """
+        Excepcion cuando un paciente no se encuentra en el sistema INCUCAI
 
-""" def _crear_objetos_organo(self, lista_organos_str):
+    """
+pass 
 
-        print(f"Error: El tipo de órgano '{organo_str}' no es válido para el donante {self.nombre}. Tipos válidos: {Organo.TIPOS_VALIDOS}")
+class Centro_Salud_No_Encontrado:
+    """
+        Excepcion cuando un centro de salud no se encuentra en el sistema INCUCAI
 
-"""
+    """
+pass 
 
-# --- ORGANO --- # 
+class Recursos_Insuficientes:
+    """
+    Excepcion cuando no se peude asignar los recursos necesarios(vehiculo/cirujano)
 
-"""
-def__init__(self, tipo_org):
-
-    if tipo_org not in self.TIPOS_VALIDOS:
-            raise ValueError(f"Tipo de órgano inválido: {tipo_org}. Los tipos válidos son: {self.TIPOS_VALIDOS}")
-        self.tipo_org = tipo_org
-        self.fecha_hora_ablacion = None
-
-"""
-
-# ---- INCUCAI ----# 
-
-""""
--Un error podria ser cuando se intenta relaizar una operacion
-que no esta registrada en el INCUCAI
-
--Podria haber un error cuadno se intenta operar con un centro
-de salud que no esta registrado, osea un centro de salud
-que no se encontro.
-Algo como Centro_salud_no_encontrado
-
--
-
-"""
-
+    """
 
 
 
